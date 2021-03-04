80c7f8d60ad35f56ff97c6dead63fa99950318f0
Itay Matza <itaymatza.outlook.com>
2021-03-04 12:45:03 +0200

    get client changed

--- messageu_client.cpp
+++ messageu_client.cpp
@@ -72,8 +72,6 @@ int main() {
 			}
 			else {
 				string username;
-				RegisterRequest* request;
-				RegisterResponse* response;
 
 				while (username.empty()) {
 					cout << "Please enter new user name: ";
@@ -82,34 +80,32 @@ int main() {
 
 				uint8_t public_key[PUBKEY_LEN];
 				GenRsaKeyPair(public_key);
-				request = encodeRegisterRequest(username, public_key);
+				RegisterRequest* request = encodeRegisterRequest(username, public_key);
 				writeToServer(sock, reinterpret_cast<uint8_t*>(request), sizeof(RegisterRequest));
-				response = readServerRegisterResponse(sock);
+				RegisterResponse* response = readServerRegisterResponse(sock);
 				writeMeInfoFile(username, response->payload.uid, &status);
 				cout << "Client registered successfully." << "\n" << endl;
 				getClientInfoFromFile(client_name, uid, private_key, &status);
 				delete request;
 				delete response;
 			}
-		}
 			break;
+		}
 		case 2:
 		{
 			cout << "Selected option 2 - Request for clients list" << endl;
-			ClientsListRequest* request;
-			ClientsListResponse* response;
 
-			request = encodeClientsListRequest(uid);
+			ClientsListRequest* request = encodeClientsListRequest(uid);
 			writeToServer(sock, reinterpret_cast<uint8_t*>(request), sizeof(ClientsListRequest));
 			cout << "MessageU - Clients list:" << endl;
 			cout << "--------------------" << endl;
-			response = readServerClientsListResponse(sock, clients_list);
+			ClientsListResponse* response = readServerClientsListResponse(sock, clients_list);
 			cout << "--------------------" << endl;
 			cout << "End of Clients list." << "\n" << endl;
 			delete request;
 			delete response;
-		}
 			break;
+		}
 		case 3:
 		{
 			cout << "Selected option 3 - Request for public key" << endl;
@@ -132,14 +128,12 @@ int main() {
 		case 4:
 		{
 			cout << "Selected option 4 - Request for waiting messages" << endl;
-			PullMessagesRequest* request;
-			PullMessagesResponse* response;
 
-			request = encodePullMessagesRequest(uid);
+			PullMessagesRequest*  request = encodePullMessagesRequest(uid);
 			writeToServer(sock, reinterpret_cast<uint8_t*>(request), sizeof(PullMessagesRequest));
 			cout << "Pulling waiting messages:" << endl;
 			cout << "-------------------------" << endl;
-			response = readServerPullMessagesResponse(sock, clients_list);
+			PullMessagesResponse* response = readServerPullMessagesResponse(sock, clients_list);
 			cout << "----------------" << endl;
 			cout << "End of messages." << "\n" << endl;
 
